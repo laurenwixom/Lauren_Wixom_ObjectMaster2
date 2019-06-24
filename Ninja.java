c1b2f11ac87a93381c793185f655c5e2a8f597d3
Lauren Wixom <50384167+laurenwixom@users.noreply.github.com>
2019-06-24 16:24:10 -0500

    Add files via upload

---
+++ Ninja.java
@@ -0,0 +1,16 @@
+package com.codingdojo.objectmaster;
+
+public class Ninja {
+	public class Ninja extends Human {
+		Integer health = 10;
+		
+		public void steal(Human h) {
+			h.health -= stealth;
+			health += stealth;
+		}
+		
+		public void runAway() {
+			health -= 10;
+		}
+	}
+}
