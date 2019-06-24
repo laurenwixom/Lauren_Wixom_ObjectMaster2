92cab32fb05709e8c9e6d92a6adc10bbb6e24b71
Lauren Wixom <50384167+laurenwixom@users.noreply.github.com>
2019-06-24 16:25:26 -0500

    Add files via upload

---
+++ Wizard.java
@@ -0,0 +1,16 @@
+
+public class Wizard {
+	public class Ninja extends Human {
+		Integer health = 10;
+		
+		public void steal(Human h) {
+			h.health -= stealth;
+			health += stealth;
+		}
+		
+		public void runAway() {
+			health -= 10;
+		}
+	}
+
+}
